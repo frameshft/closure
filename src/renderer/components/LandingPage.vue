25ac79bc769d0b930e0f919c4fb861c0a82d6820
frameshft <adiletkadyrov@gmail.com>
2020-06-01 15:27:53 +0600

    feat: add video source selection

--- src/renderer/components/LandingPage.vue
+++ src/renderer/components/LandingPage.vue
@@ -4,19 +4,51 @@
       <template v-slot:title>
         Controls
       </template>
-      <ark-button :modifiers="['outline']">Select Arknights</ark-button>
+      <ark-button :modifiers="['outline']" @click.native="getVideoSources"
+        >Select Arknights
+      </ark-button>
       <ark-button :modifiers="['action']">Analyze</ark-button>
     </ark-card>
   </div>
 </template>
 
 <script>
+  import { desktopCapturer, remote } from 'electron'
   import ArkCard from './ArkCard'
   import ArkButton from './ArkButton'
+
+  const { Menu } = remote
+
   export default {
     components: {
       ArkCard,
       ArkButton
+    },
+    data() {
+      return {
+        windowSource: {}
+      }
+    },
+    methods: {
+      async getVideoSources() {
+        console.log('Fired')
+        const inputSources = await desktopCapturer.getSources({
+          types: ['window', 'screen']
+        })
+
+        console.log(inputSources)
+
+        const videoOptionsMenu = Menu.buildFromTemplate(
+          inputSources.map(source => {
+            return {
+              label: source.name,
+              click: () => (this.windowSource = source)
+            }
+          })
+        )
+
+        videoOptionsMenu.popup()
+      }
     }
   }
 </script>
